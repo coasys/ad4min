7d846c833cbe8e83e783a13976f5b4c1ccdc4a7e
Kaicho Sun <kaichaosuna@gmail.com>
2022-03-14 18:22:40 +0800

    generate did

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
+import { useState } from 'react';
+import { Ad4mClient } from '@perspect3vism/ad4m';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
-function App() {
+const App = () => {
+
+  const [isInitialized, setIsInitialized] = useState(null);
+  const [isLocked, setIsLocked] = useState(null);
+
+  const generateAgent = async () => {
+    let ad4mClient = buildAd4mClient("ws://localhost:4000/graphql");
+    let status = await ad4mClient.agent.status();
+    console.log("agent status", status);
+  };
+
+  const renderNotInitializedContainer = () => (
+    <button onClick={generateAgent}>
+      Generate DID for agent
+    </button>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {!isInitialized && renderNotInitializedContainer()}
     </div>
   );
+}
+
+export function buildAd4mClient(server: string): Ad4mClient {
+  let apolloClient = new ApolloClient({
+    link: new WebSocketLink({
+      uri: server,
+      options: { reconnect: true },
+      webSocketImpl: WebSocket,
+    }),
+    cache: new InMemoryCache({ resultCaching: false, addTypename: false }),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "no-cache",
+      },
+      query: {
+        fetchPolicy: "no-cache",
+      }
+    },
+  });
+
+  //@ts-ignore
+  return new Ad4mClient(apolloClient);
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header>
+        <h1>Ad4min</h1>
+    </header>
+  )
+}
+
+export default Header
