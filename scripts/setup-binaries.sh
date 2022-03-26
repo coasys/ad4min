d07bd8d2e64c0d3407f27303547b5bb1fefe27ab
Kaicho Sun <kaichaosuna@gmail.com>
2022-03-26 19:22:45 +0800

    release pipeline

--- scripts/setup-binaries.sh
+++ scripts/setup-binaries.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+set -e
+
 rm -rf src-tauri/bins
 mkdir src-tauri/bins
 
 TARGET_TRIPLE=$(rustc -vV | sed -n 's/^.*host: \(.*\)*$/\1/p')
 
-wget -O src-tauri/bins/ad4m-$TARGET_TRIPLE https://github.com/perspect3vism/ad4m-host/releases/download/v0.0.4/ad4m-macos-x64
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     AD4M_HOST_BINARY=https://github.com/perspect3vism/ad4m-host/releases/download/v0.0.4/ad4m-linux-x64;;
+    Darwin*)    AD4M_HOST_BINARY=https://github.com/perspect3vism/ad4m-host/releases/download/v0.0.4/ad4m-macos-x64;;
+    *)          echo "Machine is not supported: ${unameOut}" && exit 1;;
+esac
+echo ${machine}
+
+wget -O src-tauri/bins/ad4m-$TARGET_TRIPLE $AD4M_HOST_BINARY
 
 chmod 755 src-tauri/bins/ad4m-$TARGET_TRIPLE
