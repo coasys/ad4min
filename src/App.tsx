4e9b931660a1259ffdd91d47cad2f3f01c3d580b
Kaicho Sun <kaichaosuna@gmail.com>
2022-04-01 12:58:23 +0800

    trust agent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import Header from './components/Header';
 import Login from './components/Login';
 import './App.css';
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ExceptionType } from '@perspect3vism/ad4m';
 import { ExceptionInfo } from '@perspect3vism/ad4m/lib/src/runtime/RuntimeResolver';
-import { Button, Group, Modal, TextInput, Space, Loader, Stack } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
+import { Loader, Stack } from '@mantine/core';
 import { Ad4mContext } from '.';
 import Profile from './components/Profile';
 import Language from './components/Language';
+import TrustAgent from './components/TrustAgent';
 
 const App = () => {
-
   const ad4mClient = useContext(Ad4mContext);
-  
-  const [trustCandidate, setTrustCandidate] = useState("");
-  const [opened, setOpened] = useState(false);
+
   const [connected, setConnected] = useState(false);
   const [isLogined, setIsLogined] = useState<Boolean>(false);
   const [did, setDid] = useState("");
+  const [candidate, setCandidate] = useState("");
 
   useEffect(() => {
     const checkConnection = async () => {
@@ -26,59 +24,24 @@ const App = () => {
         await ad4mClient.runtime.hcAgentInfos(); // TODO runtime info is broken
         console.log("get hc agent infos success.");
         setConnected(true);
-      } catch(err) {
+      } catch (err) {
         setConnected(false);
       }
     }
-    
+
     checkConnection();
-    
+
     console.log("Check if ad4m service is connected.")
   }, [ad4mClient]);
-
-  const addTrustedAgent = async (event: React.SyntheticEvent) => {
-    let agents = await ad4mClient.runtime.addTrustedAgents([trustCandidate]);
-    setOpened(false);
-    showNotification({
-      message: 'Great, the agent is trusted now! 🤥',
-    })
-    console.log("agent is now trusted: ", agents);
-  }
-
-  const renderTrustAgentModal = () => (
-    <div>
-      <Modal
-        opened={opened}
-        onClose={() => setOpened(false)}
-        title="Trust Agent"
-      >
-        <TextInput
-          defaultValue={trustCandidate}
-          label="Agent DID"
-        />
-        <Group>
-          <Button variant="outline" onClick={() => setOpened(false)}>
-            Close
-          </Button>
-          <Space h="md" />
-          <Button onClick={addTrustedAgent}>
-            Trust Agent
-          </Button>
-        </Group>
-
-      </Modal>
-    </div>
-  )
 
   const handleLogin = (login: Boolean, did: string) => {
     setIsLogined(login);
     setDid(did);
 
-    if(login) {
+    if (login) {
       ad4mClient.runtime.addExceptionCallback((exception: ExceptionInfo) => {
         if (exception.type === ExceptionType.AgentIsUntrusted) {
-          setTrustCandidate(exception.addon!);
-          setOpened(true);
+          setCandidate(exception.addon!);
         }
         Notification.requestPermission()
           .then(response => {
@@ -92,20 +55,22 @@ const App = () => {
     }
   }
 
+  const  handleTrustAgent = (candidate: string) => {
+    setCandidate(candidate);
+  }
+
   return (
     <div className="App">
       <Stack align="center" spacing="xl">
         <Header />
         {!connected && <Loader />}
         {connected && !isLogined && <Login handleLogin={handleLogin} />}
-        {isLogined && <Profile did={did}/>}
+        {isLogined && <Profile did={did} />}
         {isLogined && <Language />}
-        {opened && renderTrustAgentModal()}
+        {candidate && <TrustAgent candidate={candidate} handleTrustAgent={handleTrustAgent} />}
       </Stack>
     </div>
   );
 }
-
-
 
 export default App;
