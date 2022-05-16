e0a27e9f703d700207f20af9d473538e226fb967
Fayeed Pawaskar <fayeed@live.com>
2022-05-16 14:08:49 +0530

    Splash screen updated

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,6 +19,11 @@ mod system_tray;
 mod menu;
 use tauri::WindowUrl;
 use tauri::WindowBuilder;
+use tauri::api::dialog;
+use tauri::Manager;
+use directories::UserDirs;
+use std::fs;
+use crate::config::log_path;
 
 // the payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
@@ -45,22 +50,6 @@ fn main() {
         assert!(status.success());
     }
 
-    let (mut rx, _child) = Command::new_sidecar("ad4m")
-        .expect("Failed to create ad4m command")
-        .args(["serve", "--port", &free_port.to_string()])
-        .spawn()
-        .expect("Failed to spawn ad4m serve");
-
-    
-        tauri::async_runtime::spawn(async move {
-        while let Some(event) = rx.recv().await {
-            match event.clone() {
-                CommandEvent::Stdout(line) => log::info!("{}", line),
-                CommandEvent::Stderr(line) => log::info!("{}", line),
-                _ => log::info!("{:?}", event),
-            }
-        }
-    });
     let free_port_clone = free_port.clone();
 
     let url = app_url(free_port);
@@ -71,6 +60,8 @@ fn main() {
         .menu(build_menu())
         .system_tray(build_system_tray())
         .setup(move |app| {
+            let splash_screen = app.get_window("splashscreen").unwrap();
+
             let new_ad4min_window = WindowBuilder::new(
                 app,
                 "ad4min",
@@ -79,7 +70,56 @@ fn main() {
             
             log::info!("Creating ad4min UI {:?}", new_ad4min_window); 
             
-            new_ad4min_window.build();
+            let result = new_ad4min_window
+                .inner_size(1000.0, 700.0)
+                .center()
+                .visible(false)
+                .build();
+
+            let temp_result = result.unwrap().clone();
+
+            let _id = splash_screen.listen("copyLogs", |event| {
+                if let Some(user_dirs) = UserDirs::new() {
+                    let path = user_dirs.desktop_dir().unwrap().join("ad4min.log");
+                    fs::copy(log_path(), path);
+                }
+
+                println!("got window event-name with payload {:?} {:?}", event, event.payload());
+            });
+
+            let (mut rx, _child) = Command::new_sidecar("ad4m")
+            .expect("Failed to create ad4m command")
+            .args(["serve", "--port", &free_port.to_string()])
+            .spawn()
+            .expect("Failed to spawn ad4m serve");
+    
+            tauri::async_runtime::spawn(async move {
+                while let Some(event) = rx.recv().await {
+                    match event.clone() {
+                        CommandEvent::Stdout(line) => {
+                            log::info!("{}", line);
+
+                            if line == "\u{1b}[32m AD4M init complete \u{1b}[0m" {
+                                splash_screen.close().unwrap();
+                                temp_result.show().unwrap();
+                            }
+                        },
+                        CommandEvent::Stderr(line) => log::info!("{}", line),
+                        CommandEvent::Terminated(line) => {
+                            println!("Terminated {:?}", line);
+
+                            dialog::message(
+                                Some(&temp_result), 
+                                "Error", 
+                                "Something went wrong while starting ad4m-executor please check the logs"
+                            );
+                            log::info!("Terminated {:?}", line);
+                        },
+                        CommandEvent::Error(line) => log::info!("Error {:?}", line),
+                        _ => log::info!("Error {:?}", event),
+                    }
+                }
+            });
 
             Ok(())
         })
