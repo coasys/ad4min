c15ffe475afedf558d11ff2c64af7f66357021de
fayeed <fayeed@live.com>
2022-05-23 15:20:46 +0530

    Kill running ad4m-host & its child process when quitting & start the app

--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -1,3 +1,5 @@
+use sysinfo::{ProcessExt, System, SystemExt, Signal};
+
 pub fn find_port(start_port: u16, end_port: u16) -> u16 {
   for x in start_port..end_port {
     if portpicker::is_free(x) {
@@ -8,3 +10,13 @@ pub fn find_port(start_port: u16, end_port: u16) -> u16 {
   panic!("No open port found between: [{:?}, {:?}]", start_port, end_port);
 }
 
+pub fn find_and_kill_processes(name: &str) {
+  let processes = System::new_all();
+
+  for process in processes.processes_by_exact_name(name) {
+      log::info!("Prosses running: {} {}", process.pid(), process.name());
+      if process.kill_with(Signal::Term).is_none() {
+        log::error!("This signal isn't supported on this platform");
+    }
+  }
+}
